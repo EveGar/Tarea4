40cbd1de3a1a42eb2188f22aaa57126585b618fe
EveGar <89033405+EveGar@users.noreply.github.com>
2021-09-06 11:12:46 -0600

    Update exercise.py

--- assignments/02ConjeturaUlam/src/exercise.py
+++ assignments/02ConjeturaUlam/src/exercise.py
@@ -1,5 +1,18 @@
 def main():
-    pass
+    n=int(input())
+    b=str(n)
+    if n>0:
+       while(n>1):
+           if(n%2==0):
+               n=(n//2)
+               b=b+' '+str(n)
+        
+           else:
+               n=(n*3)+1
+               b=b+' '+str(n)
+       print(b)
+   else:
+       print('Invalid input')
 
 if __name__=='__main__':
     main()
